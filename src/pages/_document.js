d52fc9608a363b356b8aa543aa1ded5e7bde4616
Mohammad Bilal <mohdbilal16@gmail.com>
2023-06-21 22:26:53 +0530

    fix: title icon update

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -49,7 +49,7 @@ class MyDocument extends Document {
             content="Increase Sales Reversing Bad Publicity"
           />
           <meta name="author" content="" />
-          <link rel="shortcut icon" href="/img/fav.png" />
+          <link rel="shortcut icon" href="/img/fav.ico" />
           <link
             href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap"
             rel="stylesheet"
